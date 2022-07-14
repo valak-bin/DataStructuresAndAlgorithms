94c8b968c18ac772fb60f8bc51b452759e273aa0
jered <me@jered.dev>
2022-07-14 16:16:50 -0600

    Added binary search algorithm.
    Coded up a solution for binary search. Given a sorted array of items, find the position of any given item.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] myArr = new int[] { 1, 3, 5, 7, 9 };
+
+Console.WriteLine(BinarySearch(myArr, 9));
+Console.WriteLine(BinarySearch(myArr, -1));
+
+
+int BinarySearch(int[] list, int item)
+{
+    int low = 0;
+    int high = list.Length - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int guess = list[mid];
+
+        if (guess == item)
+        {
+            return mid;
+        }
+        else if (guess > item)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+
+    return -1;
+}
