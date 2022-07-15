9e5723a22c446ca6110f3b76d5bfca33e098d4ef
jered <me@jered.dev>
2022-07-15 15:10:09 -0600

    Added Divide and Conquer.
    Added two small recursive methods to show the Divide and Conquer technique.

---
+++ DivideAndConquer/Program.cs
@@ -0,0 +1,27 @@
+﻿// Given an array, add up all of the values in the array and return the sum.
+
+int[] myArr = new int[] { 2, 4, 6, 8, 10};
+
+Console.WriteLine($"Array sum: {ArraySum(myArr)}");
+Console.WriteLine($"Array count: {ArrayCount(myArr)}");
+
+
+int ArraySum(int[] arr)
+{
+    if (arr.Length == 0)
+    {
+        return 0;
+    }
+
+    return arr[0] + ArraySum(arr[1..]);
+}
+
+int ArrayCount(int[] arr)
+{
+    if (arr.Length == 0)
+    {
+        return 0;
+    }
+
+    return 1 + ArrayCount(arr[1..]);
+}
