715c4482c455595bef895268d1284c5faead6db3
jered <me@jered.dev>
2022-07-15 11:42:32 -0600

    Added Recursion.
    Added a couple of small examples of recursive methods.

---
+++ Recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿// Recursion is when a function calls itself.
+// Every recursive function has two cases: The base case and the recursive case.
+
+// CountdownEvent(10);
+Console.WriteLine(FactorialCalculator(10));
+
+int FactorialCalculator(int x)
+{
+    if (x == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return x * FactorialCalculator(x - 1);
+    }
+}
+
+void CountdownEvent(int x)
+{
+    Console.WriteLine(x);
+
+    if (x <= 1)
+    {
+        return;
+    }
+    else
+    {
+        CountdownEvent(x - 1);
+    }
+}
