28529fefab3cb6593299f3b157c05e6b40ad2022
jered <me@jered.dev>
2022-07-17 16:35:20 -0600

    Create HashTable project.
    Create the HashTable console application project. Added a simple dictionary example and the CheckVoter method.

---
+++ HashTables/Program.cs
@@ -0,0 +1,39 @@
+﻿// Hash Tables
+
+// A hash table is a data structure that combines a hash function
+// and an array together. A hash table has keys and values. AKA Dictionaries.
+
+// Simple Example
+Dictionary<string, decimal> productPrices = new Dictionary<string, decimal>();
+
+productPrices["apple"] = 0.67m;
+productPrices["milk"] = 1.49m;
+productPrices["avocado"] = 1.49m;
+
+Console.WriteLine(productPrices["apple"]);
+
+// Preventing Duplicate Entries
+
+// Example: Keeping track of a voter registry. You cannot vote twice.
+
+// First, create a hash to keep track of the people who have voted.
+Dictionary<string, bool> voterReceipts = new Dictionary<string, bool>();
+
+voterReceipts["Jim"] = true;
+voterReceipts["Pam"] = true;
+
+CheckVoter("Jim");
+CheckVoter("Creed");
+
+void CheckVoter(string voterName)
+{
+    if (voterReceipts.TryGetValue(voterName, out bool value))
+    {
+        Console.WriteLine("They have already voted. Do not let them vote again.");
+    }
+    else
+    {
+        voterReceipts[voterName] = true; // Add them to the voter receipts dictionary.
+        Console.WriteLine("Please let them vote.");
+    }
+}
