abae093508d644cfa53660597c26ad2db691e179
jered <me@jered.dev>
2022-07-19 17:07:29 -0600

    Create Stack, Queue, and BFS.
    Create projects for Stack and Queue data structures. Create project for algorithm Breadth-first Search.

---
+++ BreadthFirstSearch/Program.cs
@@ -0,0 +1,62 @@
+﻿// Breadth-first Search
+
+//
+
+//
+
+
+// Variables
+
+// Create an unweighted directed graph using a Dictionary
+Dictionary<string, string[]> peopleGraph = new Dictionary<string, string[]>();
+
+peopleGraph["You"] = new string[] { "Alice", "Bob", "Claire" };
+peopleGraph["Bob"] = new string[] { "Anuj", "Peggy" };
+peopleGraph["Alice"] = new string[] { "Peggy" };
+peopleGraph["Tom"] = new string[] { "Thom", "Johnny" };
+peopleGraph["Anuj"] = Array.Empty<string>();
+peopleGraph["Peggy"] = Array.Empty<string>();
+peopleGraph["Thom"] = Array.Empty<string>();
+peopleGraph["Johnny"] = Array.Empty<string>();
+peopleGraph["Claire"] = Array.Empty<string>();
+
+// Method Calls
+vendorSearch("You");
+
+// Methods
+void vendorSearch(string name)
+{
+    Queue<string> searchQueue = new Queue<string>();
+    List<string> searched = new List<string>();
+
+    foreach (var item in peopleGraph[name])
+    {
+        searchQueue.Enqueue(item);
+    }
+
+    while (searchQueue.Count > 0)
+    {
+        string person = searchQueue.Dequeue();
+        if (!searchQueue.Contains(person))
+        {
+            if (personIsAVendor(person))
+            {
+                Console.WriteLine($"{person} is a vendor.");
+            }
+            else
+            {
+                foreach (var item in peopleGraph[person])
+                {
+                    searchQueue.Enqueue(person);
+                }
+                
+                searched.Add(person);
+            }
+        }
+    }
+}
+
+bool personIsAVendor(string name)
+{
+    return name == "Thom";
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,18 @@
+﻿// Queue
+
+// Represents a first-in, first-out (FIFO) collection of objects.
+
+// Create an empty Queue
+Queue<string> hackers = new Queue<string>();
+
+// Add objects to the queue
+hackers.Enqueue("Elliot Alderson");
+hackers.Enqueue("Darlene Alderson");
+hackers.Enqueue("Angela Martin");
+
+// Remove the first object off the bottom of the queue
+// and store it in a variable
+string nextHacker = hackers.Dequeue();
+
+// Display the bottom object of the queue without removing it
+hackers.Peek();
