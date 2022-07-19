295eda2912f7d6a1c9f8c5a0aad126c4776507cd
jered <me@jered.dev>
2022-07-19 10:39:13 -0600

    Create Arrays project.
    Create a new console app project to hold array examples.

---
+++ Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿// Arrays
+
+// An array is a data structure that holds a group of one or more items
+// that are stored contiguously (right next to eachother) in memory.
+
+// Good at: reading data using random access
+// Bad at: inserting and deleting data
+
+// Integer Array
+int[] integerArray = new int[] { 18, 21, 100, 1, 69 };
+
+// String Array
+string[] stringArray = new string[] { "Elliot", "Darleen", "Angela" };
+
+// Empty Array
+// Using Array.Empty does not allocate any memory on the heap vs new int[] {};
+int[] emptyIntegerArray = Array.Empty<int>();
