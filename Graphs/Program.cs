9e9d4bebcb7191e0eb803da5ee14d1821d3f7669
jered <me@jered.dev>
2022-07-18 21:46:53 -0600

    Create Graphs.
    Create an example of a graph implemented by using a dictionary.

---
+++ Graphs/Program.cs
@@ -0,0 +1,25 @@
+﻿// Graphs
+
+// A graph models a set of connections made up of nodes and edges.
+// We can use a Dictionary to map a node to all of its neighbors.
+
+Dictionary<string, string[]> peopleGraph = new Dictionary<string, string[]>();
+
+// This represents a directed graph. To map nodes together, you'd have
+// to create an additional entry to map Anuj to Bob for example.
+
+peopleGraph["You"] = new string[] { "Alice", "Bob", "Claire" };
+peopleGraph["Bob"] = new string[] { "Anuj", "Peggy" };
+peopleGraph["Alice"] = new string[] { "Peggy" };
+peopleGraph["Tom"] = new string[] { "Thom", "Johnny" };
+peopleGraph["Anuj"] = new string[] { };
+peopleGraph["Peggy"] = new string[] { };
+peopleGraph["Thom"] = new string[] { };
+peopleGraph["Johnny"] = new string[] { };
+
+
+foreach (var item in peopleGraph["Tom"])
+{
+    Console.WriteLine(item);
+}
+
