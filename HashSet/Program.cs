7eb69c7235d010ecc847da5e0a49e11f1f745adf
jered <me@jered.dev>
2022-07-29 16:04:09 -0600

    Create new Questions folder.
    Created Questions folder to hold answers to popular questions. Also, created a new project for HashSet. Which is a different way to implement a hash table.

---
+++ HashSet/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Implementing a Hash Table with C# HashSet.
+
+// Similar to C# Dictionary<T> without the values.
+
+HashSet<int> evenNumbers = new HashSet<int> { 2, 4, 6, 8 };
+HashSet<int> oddNumbers = new HashSet<int> { 1, 3, 5, 7};
+
+
+// Add elements to the Hashset
+
+evenNumbers.Add(10);
+
+// You can use foreach in HashSet
+
+foreach (var item in evenNumbers)
+{
+    Console.WriteLine(item);
+}
+
+// Check if a HashSet contains a value
+
+Console.WriteLine(evenNumbers.Contains(1));
---
+++ TwoNumberSum/Program.cs
@@ -0,0 +1,90 @@
+﻿// Two Number Sum
+
+int[] array = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
+
+int targetSum = 10;
+
+//TwoNumberSumNested(array, targetSum);
+TwoNumberSumHash(array, targetSum);
+
+
+// Nested for loops approach
+// Time: O(n^2) Space: O(1)
+
+static int[] TwoNumberSumNested(int[] array, int targetSum)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int firstNum = array[i];
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            int secondNum = array[j];
+            if (firstNum + secondNum == targetSum)
+            {
+                return new int[] { firstNum, secondNum };
+            }
+        }
+    }
+
+    return new int[0];
+}
+
+// Hash table approach
+// Time: O(n) Space: O(n)
+
+// ! If you're in a new coding environment, make sure you include
+// using System.Collections.Generic;
+
+static int[] TwoNumberSumHash(int[] array, int targetSum)
+{
+    // Initialize our hashtable called nums
+    HashSet<int> nums = new HashSet<int>();
+
+    // Start iterating through our array
+    foreach (int num in array)
+    {
+        // Implement our equation y = target - x
+        int potentialMatch = targetSum - num;
+        // If y is present in our nums hashtable, return [x, y]
+        if (nums.Contains(potentialMatch))
+        {
+            return new int[] { potentialMatch, num };
+        }
+        // Otherwise, we just store our number in the hash table.
+        else nums.Add(num);
+        }
+
+    return new int[0];
+}
+
+// Pointers appoach
+// Time: O(n log n) Space: O(1)
+
+static int[] TwoNumberSumPointers(int[] array, int targetSum)
+{
+    // Sort our array with build-in C# sorting, assuming O(n log n) speed
+    Array.Sort(array);
+    
+    // Initilize left pointer, first value of the array
+    int left = 0;
+    
+    // Initialize right pointer, final value of the array
+    int right = array.Length - 1;
+
+    // While the two pointers don't overlap, or the left is not
+    // past the right:
+
+    while (left < right)
+    {
+        int currentSum = array[left] + array[right];
+        if (currentSum == targetSum)
+        {
+            return new int[] { array[left], array[right] };
+        }
+        else if (currentSum < targetSum) left++;
+        else if (currentSum > targetSum) right--;
+    }
+
+    return new int[0];
+}
