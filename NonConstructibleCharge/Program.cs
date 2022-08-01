82d48f7454a7d74cef89d1e1814e015411c8d940
jered <me@jered.dev>
2022-07-31 23:21:58 -0600

    Create TournamentWinner and NonConstructible Change project.
    Two more array based algorithms.

---
+++ NonConstructibleCharge/Program.cs
@@ -0,0 +1,31 @@
+﻿// Non-constructible Change
+// Time: O(n log n) Space: O(1)
+
+int NonConstructibleChange(int[] coins)
+{
+    // Start by sorting the coins array
+    Array.Sort(coins);
+
+    // Create a variable to hold the current change created
+    var currentChangeCreated = 0;
+
+    // Loop through all of the coins
+    foreach (var coin in coins)
+    {
+        // Check if the value of the coin we're adding is greater than the
+        // current change created plus one. If it is, that tells us that we've
+        // found the minimum amount of change that we cannot create.
+        if (coin > currentChangeCreated + 1) return currentChangeCreated + 1;
+
+        // Otherwise add the value of the coin to our current change created
+        // This says we can create all of the values of change up to the coin we
+        // just added.
+        currentChangeCreated += coin;
+
+    }
+
+    // If we get through the whole array, the next value we cannot create
+    // is the largest coin plus 1.
+    return currentChangeCreated + 1;
+
+}
---
+++ TournamentWinner/Program.cs
@@ -0,0 +1,57 @@
+﻿// Tournament Winner
+
+// Brute Force is the best optimized solution
+// Time: O(n) Space: O(k) where n is the number
+//      of competitons and k is the number of teams.
+
+int HOME_TEAM_WON = 1;
+
+string TournamentWinner(List<List<string>> competitions, List<int> results)
+{
+    string currentBestTeam = string.Empty;
+    
+    // Create scores data structure that keep track of all the team scores
+    Dictionary<string, int> scores = new Dictionary<string, int>();
+    scores[currentBestTeam] = 0;
+
+    // Loop through the entire competitions array
+    for (int idx = 0; idx < competitions.Count; idx++)
+    {
+        // Use idx in the for loop for the result array index
+        int result = results[idx];
+
+        // Decompose the competition into the home and away teams
+        List<string> competition = competitions[idx];      
+
+        string homeTeam = competition[0];
+        string awayTeam = competition[1];
+
+        // Determine the winner based on the results array
+        string winningTeam = (result == HOME_TEAM_WON) ? homeTeam : awayTeam;
+
+        // Update the score of the winning team
+        updateScores(winningTeam, 3, scores);
+
+        // Check if we need to update the best team based on the
+        // last winning team
+        if (scores[winningTeam] > scores[currentBestTeam])
+        {
+            currentBestTeam = winningTeam;
+        }
+    }
+
+    return currentBestTeam;
+}
+
+void updateScores(string team, int points, Dictionary<string, int> scores)
+{
+    // Check if the team is a key in our data structure
+    // If not, then add the key
+    if (!scores.ContainsKey(team))        
+    {
+        scores[team] = 0;
+    }
+
+    // Update the team score
+    scores[team] = scores[team] + points;
+}
